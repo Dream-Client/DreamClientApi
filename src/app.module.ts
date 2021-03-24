6076daee38fa2fb38c6077bf66096745ca638d95
yamiM0NSTER <jomex50@gmail.com>
2021-03-24 19:53:22 +0100

    Licence and player models

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './client/client.module';
+import { PlayerModule } from './player/player.module';
+import { LicenceModule } from './licence/licence.module';
 
 @Module({
-  imports: [ClientModule],
+  imports: [ClientModule, PlayerModule, LicenceModule],
   controllers: [AppController],
   providers: [AppService],
 })
